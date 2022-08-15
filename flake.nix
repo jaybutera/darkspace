9a4c5574fe6a6777e84a4ccccbf4aa74fae73a2a
Jay Butera <buterajay@protonmail.com>
2022-08-15 13:11:23 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Darkspace Flake";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.mozilla = { url = "github:mozilla/nixpkgs-mozilla"; flake = false; };
+
+  outputs =
+    { self
+    , nixpkgs
+    , mozilla
+    , flake-utils
+    , ...
+    } @inputs:
+    let rustOverlay = final: prev:
+          let rustChannel = prev.rustChannelOf {
+            channel = "1.63.0";
+            sha256 = "sha256-oro0HsosbLRAuZx68xd0zfgPl6efNj2AQruKRq3KA2g=";
+          };
+          in
+          { inherit rustChannel;
+            rustc = rustChannel.rust;
+            cargo = rustChannel.rust;
+          };
+    in flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import "${mozilla}/rust-overlay.nix")
+            rustOverlay
+          ];
+        };
+        in {
+          devShell = pkgs.mkShell {
+            #RUST_BACKTRACE=1;
+            #RUST_LOG="trace";
+            buildInputs = with pkgs; [
+              openssl
+              gnumake
+              openssl
+              clang
+              libclang
+              pkg-config
+              cmake
+              llvm
+              freetype
+              fontconfig
+              sfml
+              (rustChannel.rust.override { extensions = [ "rust-src" ]; })
+            ];
+            LIBCLANG_PATH="${pkgs.libclang.lib}/lib";
+          };
+        });
+}
